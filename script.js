b3cf89e1568f20cfb1e66708f450d07496884f0e
Eric Martin <eric_martin@me.com>
2021-09-14 23:09:06 -0600

    added more comments

--- script.js
+++ script.js
@@ -5,12 +5,13 @@ var confirmCharacter;
 var confrimUppercase;
 var confirmLowercase;
 // Password Value Arrays
-// Special characters
+  // Special characters
 character = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", ":", ";", "'", "<", ",", ">", ".", "?", "/", "`", "~", "{", "[", "}", "]", "|"];
-// Numeric characters
+  // Numeric characters
 number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-// Alphabetical characters
+  // Alphabetical characters
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
 // Uppercase conversion
 space = [];
 // Converts letters to uppercase
@@ -20,6 +21,7 @@ var toUpper = function (upperCase) {
 };
 alphabetUpper = alphabet.map(toUpper);
 
+// Button "Click" Event Listener
 var generateBtn = document.querySelector("#generate");
 
 generateBtn.addEventListener("click", function () {
