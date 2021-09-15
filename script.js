0a184b497f4ff4e4e8e9d73f3469578ac0cee299
Eric Martin <eric_martin@me.com>
2021-09-14 21:29:42 -0600

    created uppercase conversion function

--- script.js
+++ script.js
@@ -11,6 +11,14 @@ specialCharacter = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", 
 number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 // Alphabetical characters
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// Uppercase conversion
+space = [];
+// Converts letters to uppercase
+var choices;
+var toUpper = function (up) {
+  return up.toUpperCase();
+};
+alphabet2 = alphabet.map(toUpper);
 
 var generateBtn = document.querySelector("#generate");
 
