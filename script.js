bb57af5ffb87fc2209c062b890c996a486e01ffe
Eric Martin <eric_martin@me.com>
2021-09-14 23:07:34 -0600

    updated number array to include 0

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var confirmLowercase;
 // Special characters
 character = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "\:", "\;", "'", "<", ",", ">", ".", "?", "/", "`", "~", "{", "[", "}", "]", "|"];
 // Numeric characters
-number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 // Alphabetical characters
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 // Uppercase conversion
